9e638457a1c540f3238d3ab88715dcdc194a529a
gabrego41 <gas.abrego@gmail.com>
2022-07-25 21:45:00 -0300

    Create DesafioEntregable1.cs
    Desafio Entregable 1 , Solo creacion de objetos y declaracion de variables

---
+++ DesafioEntregable1.cs
@@ -0,0 +1,47 @@
+﻿namespace DesafioEntregable
+{
+    class ProbarObjetos
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class Usuario
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string nombreUsuario;
+        private string password;
+        private string mail;
+    }
+
+    public class Producto
+    {
+        private int id;
+        private string descripcion;
+        private double costo;
+        private double precioVenta;
+        private int stock;
+        private int idUsuario;
+
+    }
+    public class ProductoVendido
+    {
+        private int id;
+        private string idProducto;
+        private int stock;
+        private int idVenta;
+
+    }
+    public class Venta
+    {
+        private int id;
+        private string comentario;
+
+    }
+
+
+}
